7addf70d186090da3053f81a71f4e34250168ef3
Md. Injamam Hossain <injamam075@gmail.com>
2025-03-28 10:32:05 +0600

    pointer learning

---
+++ first-program/pointer.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	//pointer or address memory(ram)
+	x := 50
+
+	p := &x
+
+	fmt.Println(x)
+	fmt.Println(p)
+
+}
+
+/*
+2.Phases:
+
+	1.Compilation Phase (compile time)
+	2.Execution Phase (run time)
+
+
+***********Compile Phase ********
+
+***Code Segment***
+*/
