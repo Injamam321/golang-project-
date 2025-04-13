c81040f78f8ab3d0b8b9816f2bb1b5a624fc0d53
Md. Injamam Hossain <injamam075@gmail.com>
2025-04-13 13:39:27 +0600

    pointer learning

--- first-program/pointer.go
+++ first-program/pointer.go
@@ -2,14 +2,41 @@ package main
 
 import "fmt"
 
+type User struct {
+	Name   string
+	Age    int
+	Salary float64
+}
+
+// func print(numbers *[3]int) {
+// 	fmt.Println(numbers)
+// }
+
 func main() {
+	Imjamam := User{ //instance or obj
+		Name:   "Siam",
+		Age:    21,
+		Salary: 29,
+	}
+
+	x := &Imjamam
+
+	fmt.Println(*x)
+
 	//pointer or address memory(ram)
-	x := 50
+	// x := 50
+	// fmt.Println("x =", x) // x=50
 
-	p := &x
+	// p := &x //ampersand & => address of |  824,633,755,368
+	// *p = 70
 
-	fmt.Println(x)
-	fmt.Println(p)
+	// fmt.Println("x =", x)
+
+	// fmt.Println("Address: ", p)               //p is the address of x
+	// fmt.Println("Value at the address: ", *p) // * => value at address
+
+	// arr := [3]int{1, 2, 3}
+	// print(&arr)
 
 }
 
