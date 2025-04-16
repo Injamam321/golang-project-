ac6e8b8650198fd8dbc5c8aef58c27f18cc031c2
Md. Injamam Hossain <injamam075@gmail.com>
2025-04-16 10:11:42 +0600

    slice learning part-3

--- first-program/slice.go
+++ first-program/slice.go
@@ -3,14 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	arr := [9]string{"i", "love", "Go", "programming", "language", "i", "want", "remote", "job"}
+	arr := [9]string{"i", "love", "Go", "programming", "language", "i", "want", "remote", "job"} // BODY
 	fmt.Println(arr)
 
 	s := arr[1:5]
-	fmt.Println(s) // [love go programming language]
+	fmt.Println(s) // [love, Go, programming, language] // HAAT
 
 	s1 := s[1:3]
-	fmt.Println(s1)
+	fmt.Println(s1)      // [Go, programming] //RING
+	fmt.Println(len(s1)) //length ->2
+	fmt.Println(cap(s1)) //
 }
 
 /*
@@ -25,6 +27,9 @@ func main() {
 ***Code Segment***
 main=func(){...}
 
+*/
 
-
+/*
+1.slice from an existing array
+2.slice from an slice
 */
